98053a446431a3770c62ca967ac26c069ee23b47
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-19 11:39:06 -0300

    Cambiando los import del proyecto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
 
 function App() {
   return (
