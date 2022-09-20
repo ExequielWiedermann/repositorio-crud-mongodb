6908f77cbf711adc6fa870f66c3a1c5060f09168
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-20 17:15:45 -0300

    Cargando la configuración de firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,22 @@
-export default {
-    apiKey: "AIzaSyA4v0E23-dZK9HXskasdrfsaZJNLS34FGQ",
-    authDomain: "projectId.firebaseapp.com",
-    databaseURL: "https://projectId.firebaseio.com",
-    projectId: "albumes-facilito-react",
-    storageBucket: "aprojectId.appspot.com",
-    messagingSenderId: "12356789101",
-    appId: "1:123456789101:web:3123be123456a123a456789"
-};
+import * as firebase from "firebase";
+import "firebase/auth";
+//import dataConfig from './dataConfig'
+
+const app = firebase.initializeApp(
+    {
+        apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        projectId: "XXXXXXXXXXXXXXXX",
+        storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXX",
+        messagingSenderId: "XXXXXXXXXXXXXXXXXXXX",
+        appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    }
+);
+
+/*const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+const githubAuthProvider = new firebase.auth.GithubAuthProvider();*/
+
+export default app;
